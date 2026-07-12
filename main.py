6cb1b764e74c72f25f72762a473f36230873728f
Kamran Shaikh <kbshaikh2596@gmail.com>
2026-07-12 11:23:21 +0000

    groq AI changes

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 import uvicorn
 import asyncio
+from langgraph.graph import StateGraph, START, END
+from typing import Optional, TypedDict, Literal
 # Call it to load variables from a .env file into os.environ
 load_dotenv()
 
@@ -20,31 +22,28 @@ tool = TavilySearch(
 from langchain.agents import create_agent
 from langchain_tavily import TavilySearch
 from langchain_ollama import ChatOllama
+from langchain_groq import ChatGroq
 
-llm = ChatOllama(
-    #model="qwen3.5:0.8b",
-    model="qwen3.5:2b",
-    temperature=0,
-    # other params...
+# llm = ChatOllama(
+#     #model="qwen3.5:0.8b",
+#     model="qwen3.5:2b",
+#     temperature=0,
+#     # other params...
+# )
+llm = ChatGroq(
+    model="openai/gpt-oss-120b",
+    temperature=0
 )
-
 # Initialize the Tavily Search tool
 tavily_search = TavilySearch(max_results=5, topic="general")
 
+class agent_state(TypedDict):
+    message: str 
 
-async def demo_agent(user_message:str):
-#Initialize the agent with the search tool 
-    agent = create_agent(
-        model=llm,
-        tools=[tavily_search],
-        system_prompt="You are a helpful research assistant. Use web search to find accurate, up-to-date information."
-    )
 
-    # Use the agent
-    response = agent.invoke({
-        "messages": [{"role": "user", "content": user_message}]
-    })
-    return response
+def llm_call(state: agent_state):
+      response = llm.invoke(state["message"])
+      return {"message":response.content}
 
 
 
@@ -53,9 +52,13 @@ app =FastAPI()
 
 @app.post("/llm-chat")
 async def llm_chat(user_message:str):
-    respo = await (demo_agent(user_message))
-    return respo["messages"][1].content
-
+    agent_graph = StateGraph(agent_state)
+    agent_graph.add_node("llm-call",llm_call)
+    agent_graph.add_edge(START,"llm-call")
+    agent_graph.add_edge("llm-call",END)
+    agent_graph= agent_graph.compile()
+    respo = agent_graph.invoke({"message":user_message})
+    return respo["message"]
 
 
 if __name__ == "__main__":
--- ollama_setup.sh
+++ ollama_setup.sh
@@ -2,3 +2,6 @@ curl -fsSL https://ollama.com/install.sh | sh
 #ollama pull qwen3.5:0.8b
 ollama pull qwen3.5:2b
 ollama list
+
+# git config --global user.email "kbshaikh2596@gmail.com"
+# git config --global user.name "Kamran Shaikh"
